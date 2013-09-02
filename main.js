82f8cdf3e3ad55bdae89a99b7e46facb51990daf
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-02 17:14:59 +0800

    Directive restrictions

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ var app = angular.module("superhero", []);
 
 app.directive('superman', function() {
 	return {
-		restrict: "E",
-		template: "<div>Here I am to save the day</div>"		
+		restrict: "A", // A = Attribute, C = Class, E = Element, M = Comment
+		link: function() {
+			alert("Wooo");
+		}		
 	}
 })
