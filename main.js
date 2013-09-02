d3a644c7dface034f29d33428ff2ddd9d21bf0ba
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-02 14:03:36 +0800

    Using ng-repeat and angular built-in search filter

--- main.js
+++ main.js
@@ -1,24 +1,62 @@
 var myApp = angular.module('myApp', []);
 
-myApp.factory('Data', function() {
-	return {message: "I'm data from a service"};
+myApp.factory('Avengers', function() {
+    var Avengers = {};
+
+    Avengers.cast = [{
+        "Character": "Tony Stark / Iron Man",
+        "Name": "Robert Downey Jr."
+    }, {
+        "Character": "Steve Rogers / Captain America",
+        "Name": "Chris Evans"
+    }, {
+        "Character": "Bruce Banner / The Hulk",
+        "Name": "Mark Ruffalo"
+    }, {
+        "Character": "Thor",
+        "Name": "Chris Hemsworth"
+    }, {
+        "Character": "Natasha Romanoff / Black Widow",
+        "Name": "Scarlett Johansson"
+    }, {
+        "Character": "Clint Barton / Hawkeye",
+        "Name": "Jeremy Renner"
+    }, {
+        "Character": "Loki",
+        "Name": "Tom Hiddleston"
+    }, {
+        "Character": "Agent Phil Coulson",
+        "Name": "Clark Gregg"
+    }, {
+        "Character": "Agent Maria Hill",
+        "Name": "Cobie Smulders"
+    }, {
+        "Character": "Selvig",
+        "Name": "Stellan Skarsgard"
+    }, {
+        "Character": "Nick Fury",
+        "Name": "Samuel L. Jackson"
+    }, {
+        "Character": "Pepper Potts",
+        "Name": "Gwyneth Paltrow"
+    }, {
+        "Character": "Jarvis (voice)",
+        "Name": "Paul Bettany"
+    }, {
+        "Character": "The Other",
+        "Name": "Alexis Denisof"
+    }, {
+        "Character": "NASA Scientist",
+        "Name": "Tina Benko"
+    }];
+
+    return Avengers;
+
 })
 
 
-myApp.filter('reverse', function(){
-	return function(text) {
-		return text.split("").reverse().join("");
-	}
-})
 
-function FirstCtrl($scope, Data) {
-	$scope.data = Data;
-}
+function AvengersCtrl($scope, Avengers) {
+    $scope.avengers = Avengers;
 
-function SecondCtrl($scope, Data) {
-	$scope.data = Data;
-
-	// $scope.reversedMessage = function(message) {
-	// 	return message.split("").reverse().join("");
-	// }
 }
