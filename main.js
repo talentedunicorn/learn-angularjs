accfb4ece1b5531957ba4c795c89510a6e074581
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-02 17:09:21 +0800

    Directives example

--- main.js
+++ main.js
@@ -1,62 +1,8 @@
-var myApp = angular.module('myApp', []);
+var app = angular.module("superhero", []);
 
-myApp.factory('Avengers', function() {
-    var Avengers = {};
-
-    Avengers.cast = [{
-        "Character": "Tony Stark / Iron Man",
-        "Name": "Robert Downey Jr."
-    }, {
-        "Character": "Steve Rogers / Captain America",
-        "Name": "Chris Evans"
-    }, {
-        "Character": "Bruce Banner / The Hulk",
-        "Name": "Mark Ruffalo"
-    }, {
-        "Character": "Thor",
-        "Name": "Chris Hemsworth"
-    }, {
-        "Character": "Natasha Romanoff / Black Widow",
-        "Name": "Scarlett Johansson"
-    }, {
-        "Character": "Clint Barton / Hawkeye",
-        "Name": "Jeremy Renner"
-    }, {
-        "Character": "Loki",
-        "Name": "Tom Hiddleston"
-    }, {
-        "Character": "Agent Phil Coulson",
-        "Name": "Clark Gregg"
-    }, {
-        "Character": "Agent Maria Hill",
-        "Name": "Cobie Smulders"
-    }, {
-        "Character": "Selvig",
-        "Name": "Stellan Skarsgard"
-    }, {
-        "Character": "Nick Fury",
-        "Name": "Samuel L. Jackson"
-    }, {
-        "Character": "Pepper Potts",
-        "Name": "Gwyneth Paltrow"
-    }, {
-        "Character": "Jarvis (voice)",
-        "Name": "Paul Bettany"
-    }, {
-        "Character": "The Other",
-        "Name": "Alexis Denisof"
-    }, {
-        "Character": "NASA Scientist",
-        "Name": "Tina Benko"
-    }];
-
-    return Avengers;
-
+app.directive('superman', function() {
+	return {
+		restrict: "E",
+		template: "<div>Here I am to save the day</div>"		
+	}
 })
-
-
-
-function AvengersCtrl($scope, Avengers) {
-    $scope.avengers = Avengers;
-
-}
