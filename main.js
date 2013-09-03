6bc1e773010f3bb86c6bc58316ee31adc0f3fb56
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-03 15:20:31 +0800

    Other ways to organize controllers, directives..etc

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 var app = angular.module("myApp", []);
 
+var controllers = {}
 
-app.controller("AppCtrl", function($scope) {
+controllers.AppCtrl = function($scope) {
 	
 	this.sayHi = function () {
 		alert('Hi!');
 	}
 
 	return $scope.AppCtrl = this;
-})
+}
+
+app.controller(controllers);
+
+
+var directives = {}
+
+directives.panel = function() {
+	return {
+		restrict: "E",
+		template: '<h1>Panel</h1>'
+	}
+}
+
+app.directive(directives);
