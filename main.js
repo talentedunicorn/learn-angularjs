1f82a6c0e5776e888e26ade064ba6d58a1de3b1f
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-03 13:31:36 +0800

    Directives to directives communication

--- main.js
+++ main.js
@@ -1,15 +1,57 @@
-var app = angular.module('twitterApp', []);
+var app = angular.module('superApp', []);
 
-app.controller("AppCtrl", function($scope) {
-	$scope.loadMoreTweets = function () {
-		alert("Loading Tweets");
+app.directive("superhero", function() {
+	return {
+		restrict: "E", 
+		scope: {},
+		controller: function ($scope) {
+			$scope.abilities = []
+
+			this.addStrength = function () {
+				$scope.abilities.push("strength")
+			}
+
+			this.addSpeed = function () {
+				$scope.abilities.push("speed")
+			}
+
+			this.addFlight = function () {
+				$scope.abilities.push("flight")
+			}
+		},
+
+		link: function(scope, element) {
+			element.addClass("btn btn-primary")
+			element.bind("mouseenter", function() {
+				console.log(scope.abilities);
+			})
+		}
+	}
+});
+
+app.directive("strength", function() {
+	return {
+		require: "superhero", 
+		link: function(scope, element, attrs, superheroCtrl) {
+			superheroCtrl.addStrength();
+		}
 	}
 })
 
-app.directive("enter", function() {
-	return function(scope, element, attrs) {
-		element.bind("mouseenter", function(){
-			scope.loadMoreTweets();
-		})
+app.directive("speed", function() {
+	return {
+		require: "superhero", 
+		link: function(scope, element, attrs, superheroCtrl) {
+			superheroCtrl.addSpeed();
+		}
+	}
+})
+
+app.directive("flight", function() {
+	return {
+		require: "superhero", 
+		link: function(scope, element, attrs, superheroCtrl) {
+			superheroCtrl.addFlight();
+		}
 	}
 })
