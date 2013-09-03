796a6d9e7da2168f75eb56cf9d6e181a12cb5143
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-03 15:04:59 +0800

    Transclusion basics

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-var app = angular.module("phoneApp", []);
+var app = angular.module("myApp", []);
 
 
 app.controller("AppCtrl", function($scope) {
-	$scope.callHome = function (message) {
-		console.log("I called home and left this message: " + message);
-	}
+	
 })
 
-app.directive("phone", function(){
+app.directive("panel", function(){
 	return {
 		restrict: "E",
-		template: "<input type='text' ng-model='value' />" + "<div class='button' ng-click='dial({message:value})'>Call Home!</div>",
-		scope: {
-			dial: "&" // Passes from directive to controller even in isolated scopes
-		}
+		transclude: true, // Append DOM contents inside element 
+		template: '<div class="panel" ng-transclude>This is my panel</div>'
 	}
 })
