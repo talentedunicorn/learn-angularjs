5bfd23a04fd222c0f9523c6ecf72f46f096dfcdf
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-03 11:14:02 +0800

    Behaviors with directives

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-var app = angular.module("superhero", []);
+var app = angular.module("behaviorApp", []);
 
-app.directive('superman', function() {
-	return {
-		restrict: "A", // A = Attribute, C = Class, E = Element, M = Comment
-		link: function() {
-			alert("Wooo");
-		}		
+app.directive("enter", function() {
+	return function (scope, element) {
+		element.bind("mouseenter", function(){
+			console.log("I'm inside of you!");
+		})
+	}
+})
+
+app.directive("leave", function() {
+	return function (scope, element) {
+		element.bind("mouseleave", function(){
+			console.log("Leaving...");
+		})
 	}
 })
