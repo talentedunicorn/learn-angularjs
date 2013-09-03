47a671a66de38144e7132d9fa1299644029e3b9b
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-03 15:09:26 +0800

    Alternative way to use controllers

--- main.js
+++ main.js
@@ -3,12 +3,9 @@ var app = angular.module("myApp", []);
 
 app.controller("AppCtrl", function($scope) {
 	
-})
-
-app.directive("panel", function(){
-	return {
-		restrict: "E",
-		transclude: true, // Append DOM contents inside element 
-		template: '<div class="panel" ng-transclude>This is my panel</div>'
+	this.sayHi = function () {
+		alert('Hi!');
 	}
+
+	return $scope.AppCtrl = this;
 })
