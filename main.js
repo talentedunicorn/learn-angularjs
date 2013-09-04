6134b8a7de0c4c5a8bd6bd65d36044580e324121
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-04 16:17:41 +0800

    Resolving promises

--- main.js
+++ main.js
@@ -5,28 +5,20 @@ app.config(function($routeProvider) {
 		.when('/', 
 		{
 			templateUrl: "view/app.html",
-			controller: "AppCtrl"
+			controller: "AppCtrl",
+			resolve: {
+				app: function ($q, $timeout) {
+					var defer = $q.defer();
+					$timeout(function(){
+						defer.resolve();
+					}, 2000); // Wait 2 seconds then resove the promise
+					return defer.promise;
+				}
+			}
 		})
 })
 
 app.controller("AppCtrl", function($scope, $q){
-
-	var defer = $q.defer();
-
-	defer.promise
-	.then(function(weapon) {
-		alert("You can have my " + weapon)
-		return "axe"
-	})
-	.then(function(weapon) {
-		alert("And my " + weapon)
-		return "sword"
-	})
-	.then(function(weapon) {
-		alert("Also my " + weapon)
-	})
-
-	defer.resolve("Gun");
 
 	$scope.model = {
 		message: "I am an APP o.O"
