1bffaaec0da710c7203210f86573e6e501a61805
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-04 10:37:10 +0800

    Difference between  vs scope

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 var app = angular.module("app", []);
-app.directive("dumbpassword", function() {
-	var validElement = angular.element("<div>{{model.input}}</div>");
 
-	var link = function(scope) {
-		scope.$watch("model.input", function (value) {
-			if(value == "password") {
-				validElement.toggleClass("alert-box alert");
-			}
-		})
-	}
+app.controller("AppCtrl", function($scope) {
+	console.log("Controller scope: ", $scope);
+})
 
+app.directive("mydirective", function() {
 	return {
-		restrict: "E",
-		replace: true,
-		template: "<div><input type='text' ng-model='model.input' /></div>",
-		compile: function (tElem) { // tElem = The template element
-			tElem.append(validElement);
-
-			return link;
+		link: function(scope, element, attrs) {
+			console.log("Directive scope: ", scope);
+			console.log("Directive element: ", element);
 		}
 	}
-});
+})
+
+// '$scope' in the controller is a global provider while 'scope' can be renamed to anything and still work
