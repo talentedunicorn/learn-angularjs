a5bbf8684cf7b4b09ea61c495af906428d0bb8c6
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-04 12:44:54 +0800

    Intro to routeProvider

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 var app = angular.module("myApp", []);
 
 app.config(function($routeProvider) {
-	$routeProvider.when('/', 
+	$routeProvider
+		.when('/', 
 		{
 			templateUrl: "view/app.html",
 			controller: "AppCtrl"
-		}
-	)
+		})
+		.when('/pizza', {
+			template: "Yum!!"
+		})
+		.otherwise({
+			// redirectTo: "/"
+			template: "You on the wrong side of the moon!"
+		})
 })
 
 app.controller("AppCtrl", function($scope, $route){
