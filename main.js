35f1c7b7c8f2485e6858a2028b0479e8f40ea038
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-04 16:27:07 +0800

    Resolve conventions

--- main.js
+++ main.js
@@ -7,20 +7,34 @@ app.config(function($routeProvider) {
 			templateUrl: "view/app.html",
 			controller: "AppCtrl",
 			resolve: {
-				app: function ($q, $timeout) {
-					var defer = $q.defer();
-					$timeout(function(){
-						defer.resolve();
-					}, 2000); // Wait 2 seconds then resove the promise
-					return defer.promise;
-				}
+				loadData: appCtrl.loadData,
+				prepData: appCtrl.prepData
 			}
 		})
 })
 
-app.controller("AppCtrl", function($scope, $q){
-
+var appCtrl = app.controller("AppCtrl", function($scope, $route){
+	console.log($route)
+	
 	$scope.model = {
 		message: "I am an APP o.O"
 	} 
 })
+
+appCtrl.loadData = function ($q, $timeout) {
+	var defer = $q.defer();
+	$timeout(function(){
+		defer.resolve("loadData");
+		// console.log()
+	}, 2000); // Wait 2 seconds then resove the promise
+	return defer.promise;
+}
+
+appCtrl.prepData = function ($q, $timeout) {
+	var defer = $q.defer();
+	$timeout(function(){
+		defer.resolve("prepData");
+		// console.log()
+	}, 2000); // Wait 2 seconds then resove the promise
+	return defer.promise;
+}
