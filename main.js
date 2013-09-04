818fc6b7a0b05626d0598c76cfca650d562be935
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-04 17:23:32 +0800

    Intro to providers

--- main.js
+++ main.js
@@ -1,52 +1,25 @@
 var app = angular.module("myApp", []);
 
-app.config(function($routeProvider) {
-	$routeProvider
-		.when('/', 
-		{
-			templateUrl: "view/app.html",
-			controller: "ViewCtrl",
-			resolve: {
-				loadData: viewCtrl.loadData
+
+app.config(function($provide){
+	$provide.provider("game", function(){
+		return {
+			$get: function() {
+				return {
+					title: "Super Meat Boy!"
+				}	
 			}
-		})
-})
-
-
-app.directive("error", function($rootScope){
-	return {
-		restrict: "E", 
-		template: "<div class='alert-box alert' ng-show='isError'>Error 666: The world is ending!!</div>",
-		link: function(scope) {
-			$rootScope.$on("$routeChangeError", function(event, current, previous, rejection){
-				scope.isError = true;
-			})
 		}
-	}
-})
-
-app.controller("AppCtrl", function($rootScope){ // Don't use $rootScope unless it's using an angular component
-	$rootScope.$on("$routeChangeError", function(event, current, previous, rejection) {
-		console.log("Event: ", event)
-		console.log("Current: ", current)
-		console.log("Previous: ", previous)
-		console.log("Rejection: ", rejection)
 	})
 })
 
-var viewCtrl = app.controller("ViewCtrl", function($scope, $route){
-	console.log($route)
 
-	$scope.model = {
-		message: "I am an APP o.O"
-	} 
+// app.factory("game", function(){
+// 	return {
+// 		title: "Super Meat Boy!"
+// 	}
+// })
+
+app.controller("AppCtrl", function($scope, game){ 
+	$scope.title = game.title
 })
-
-viewCtrl.loadData = function ($q, $timeout) {
-	var defer = $q.defer();
-	$timeout(function(){
-		// defer.resolve();
-		defer.reject();
-	}, 1000); // Wait 2 seconds then resove the promise
-	return defer.promise;
-}
