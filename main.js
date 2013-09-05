25369d451198cc1461e2df4bfa41b7e23ead602e
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-05 14:37:47 +0800

    Communications between directives

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 var app = angular.module("app", []);
 
-app.controller("RoomCtrl", function(){
-	this.openDoor = function () {
-		alert("creak")
+app.directive("country", function(){
+	return {
+		restrict: "E",
+		controller: function () {
+			this.makeAnnouncement = function (message) {
+				console.log("Country is saying: ", message);
+			}
+		}
 	}
 })
+
+
+app.directive("state", function(){
+	return {
+		restrict: "E",
+		controller: function () {
+			this.makeLaw = function (law) {
+				console.log("Law:", law);
+			}
+		}
+	}
+})
+
+
+
+app.directive("city", function(){
+	return {
+		restrict: "E",
+		require: ["^country","^state"],
+		link: function (scope, element, attrs, ctrls) {
+			ctrls[0].makeAnnouncement("Eye <3 this city");
+			ctrls[1].makeLaw("Clean city");
+		}
+	}
+})
+
