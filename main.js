dae6aafb8aaef322c77148a1b7d700eb261fcf16
Ezekiel Mangalu <emangalu@gmail.com>
2013-09-05 15:00:33 +0800

    Using ng-repeat-start

--- main.js
+++ main.js
@@ -1,38 +1,5 @@
 var app = angular.module("app", []);
 
-app.directive("country", function(){
-	return {
-		restrict: "E",
-		controller: function () {
-			this.makeAnnouncement = function (message) {
-				console.log("Country is saying: ", message);
-			}
-		}
-	}
+app.controller("AppCtrl", function(){
+	this.things = ['one', 'two', 'three', 'four'];
 })
-
-
-app.directive("state", function(){
-	return {
-		restrict: "E",
-		controller: function () {
-			this.makeLaw = function (law) {
-				console.log("Law:", law);
-			}
-		}
-	}
-})
-
-
-
-app.directive("city", function(){
-	return {
-		restrict: "E",
-		require: ["^country","^state"],
-		link: function (scope, element, attrs, ctrls) {
-			ctrls[0].makeAnnouncement("Eye <3 this city");
-			ctrls[1].makeLaw("Clean city");
-		}
-	}
-})
-
